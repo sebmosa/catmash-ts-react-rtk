143a15d49ffbb353f4aa4afc2f3f30b10189bb12
Sébastien Mosagna <sebmosa@gmail.com>
2023-07-20 17:04:03 +0200

    [Boilerplate] add react-router-dom, configure app and results routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,26 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
-import App from "./App"
+import App from "./pages/App"
 import "./index.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { Results } from "./pages/Results"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "results",
+    element: <Results />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import { Catmash } from "../features/catmash/Catmash.js"
+
+function App() {
+  return (
+    <div className="App">
+      <main className="main">
+        <h1 className="title">CAT MASH</h1>
+        <Catmash />
+      </main>
+    </div>
+  )
+}
+
+export default App

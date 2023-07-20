4d45d56c914038d4e4526b35f41941512436c519
Sébastien Mosagna <sebmosa@gmail.com>
2023-07-20 16:21:44 +0200

    [Feature] add catmash feature, configure rtk slice

---
+++ src/features/catmash/catmashSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
+export interface Winner {
+  id: string
+  url: string
+  points: number
+}
+
+export interface CatmashState {
+  winners: Winner[]
+  counter: number
+}
+
+const initialState: CatmashState = {
+  winners: [],
+  counter: 0,
+}
+
+const catmashSlice = createSlice({
+  name: "cat",
+  initialState,
+  reducers: {
+    won: (state, action: PayloadAction<Winner>) => {
+      const existing = state.winners.find((cat) => cat.id === action.payload.id)
+      if (existing) {
+        existing.points += 1
+      } else {
+        state.winners.push({
+          id: action.payload.id,
+          url: action.payload.url,
+          points: 1,
+        })
+      }
+    },
+    counted: (state) => {
+      state.counter += 1
+    },
+  },
+})
+
+export const { won, counted } = catmashSlice.actions
+
+export default catmashSlice.reducer

7bc46668e9f8cd29074b5c35fce9c8f325bc9916
Sébastien Mosagna <sebmosa@gmail.com>
2023-10-04 12:12:58 +0200

    enhance responsiveness

--- src/components/CatCard/CatCard.tsx
+++ src/components/CatCard/CatCard.tsx
@@ -8,6 +8,8 @@ export const CatCard = ({ image, alt }: Card) => {
   return (
     <img
       className={styles.image}
+      width={400}
+      height={300}
       src={image}
       alt={`random cat, it's cat ID:${alt}`}
     />
